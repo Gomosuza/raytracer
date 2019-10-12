fde52ef93eaaa3a9719d4458385fd4f99d082da9
Marc Stan <marcstan@live.com>
2019-10-12 21:25:27 +0200

    fix aspect ratio distortion

--- Raytracer/Scene/Camera/FpsCamera.cs
+++ Raytracer/Scene/Camera/FpsCamera.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Raytracer.Scene.Camera
 {
     public class FpsCamera : ICamera
     {
         private readonly Vector3 _lookAt;
+        private readonly GraphicsDevice _graphicsDevice;
 
-        public FpsCamera(Vector3 pos, Vector3 lookAt)
+        public FpsCamera(
+            GraphicsDevice graphicsDevice,
+            Vector3 pos,
+            Vector3 lookAt)
         {
             Position = pos;
             _lookAt = lookAt;
+            _graphicsDevice = graphicsDevice;
         }
 
         public Vector3 Position { get; }
@@ -17,8 +23,10 @@ namespace Raytracer.Scene.Camera
         public Ray GetRayForRasterPosition(int x, int y, int width, int height)
         {
             // x is in range 0 - width, we need it to be -1 to 1
-            var scalarOffsetX = -1f + x / (float)width * 2f;
             // y is in range 0 - height, we need it to be -1 to 1
+            // also consider aspectRatio to prevent view distortion
+            var aspectRatio = _graphicsDevice.Viewport.AspectRatio;
+            var scalarOffsetX = (-1f + x / (float)width * 2f) * aspectRatio;
             // needs to be the inverse of x since x is left to right both in world and raster space
             // but -1 y is down in world space but up in raster space
             var scalarOffsetY = 1f - y / (float)height * 2f;
